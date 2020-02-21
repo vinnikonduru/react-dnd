ab7c3d2ce9c5e04b37632a24c41da551a61ddb25
Vinitha Konduru <vinithakonduru@gmail.com>
2020-02-21 01:11:14 -0600

    Updates

---
+++ precache-manifest.3ea42077b7bf19c7b461731cf24ea5a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b91b1a324f8ea353d1d114d0485dba1",
+    "url": "/react-dnd/index.html"
+  },
+  {
+    "revision": "fe23a51a9734c7e0a877",
+    "url": "/react-dnd/static/css/main.8b2cdc5a.chunk.css"
+  },
+  {
+    "revision": "4736148bafcef28ef7e3",
+    "url": "/react-dnd/static/js/2.8f5c4f11.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-dnd/static/js/2.8f5c4f11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe23a51a9734c7e0a877",
+    "url": "/react-dnd/static/js/main.d578ba50.chunk.js"
+  },
+  {
+    "revision": "dbd62100ade7d788641c",
+    "url": "/react-dnd/static/js/runtime-main.cf45d206.js"
+  }
+]);
